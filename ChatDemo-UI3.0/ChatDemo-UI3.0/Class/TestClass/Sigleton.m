dae60108129297de7b0e8e5c6f48d4a0d4fd9cf3
Joe <ljm@kingmv.com>
2016-01-09 17:55:52 +0800

    update the code

---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/Class/TestClass/Sigleton.m
@@ -0,0 +1,13 @@
+//
+//  Sigleton.m
+//  ChatDemo-UI3.0
+//
+//  Created by xu yi min on 16/1/9.
+//  Copyright © 2016年 xu yi min. All rights reserved.
+//
+
+#import "Sigleton.h"
+
+@implementation Sigleton
+
+@end
